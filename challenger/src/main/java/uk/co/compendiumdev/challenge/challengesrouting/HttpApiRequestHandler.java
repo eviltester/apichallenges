9adb9e2e8dc2b7cc815b5b23c9ca7909dbe79b5a
alan <alan@compendiumdev.co.uk>
2021-08-07 22:15:47 +0100

    refactoring into the thingifier from challenges

--- challenger/src/main/java/uk/co/compendiumdev/challenge/challengesrouting/HttpApiRequestHandler.java
+++
@@ -1,9 +0,0 @@
-package uk.co.compendiumdev.challenge.challengesrouting;
-
-import uk.co.compendiumdev.thingifier.api.http.HttpApiRequest;
-import uk.co.compendiumdev.thingifier.api.response.ApiResponse;
-
-public interface HttpApiRequestHandler {
-
-    ApiResponse handle(final HttpApiRequest myRequest);
-}
